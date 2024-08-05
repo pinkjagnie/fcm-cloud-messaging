a64ac89e885a6a0803232fe43f7bcb8f67ee429b
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-05 22:14:08 +0200

    Fixed running toast in the background

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,29 +8,59 @@ self.importScripts(
 
 // Initialize the Firebase app in the service worker
 // "Default" Firebase configuration (prevents errors)
-const defaultConfig = {
-  apiKey: true,
-  projectId: true,
-  messagingSenderId: true,
-  appId: true,
-};
+// const defaultConfig = {
+//   apiKey: true,
+//   projectId: true,
+//   messagingSenderId: true,
+//   appId: true,
+// };
 
-firebase.initializeApp(defaultConfig);
+// firebase.initializeApp(defaultConfig);
 
 // Retrieve firebase messaging
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
+
+fetch("/firebase-config.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsContent) => {
+    const config = eval(jsContent);
+    firebase.initializeApp(config.firebaseConfig);
+    const messaging = firebase.messaging();
+
+    messaging.onBackgroundMessage(function (payload) {
+      // console.log(defaultConfig);
+      console.log("Received background message");
+      console.log(payload);
+
+      const notificationTitle = payload.notification.title;
+      const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image,
+      };
+
+      self.registration.showNotification(
+        notificationTitle,
+        notificationOptions
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Error initializing Firebase in service worker:", error);
+  });
 
 //Listens for background notifications
-messaging.onBackgroundMessage((payload) => {
-  console.log(defaultConfig);
-  console.log("Received background message");
-  console.log(payload);
+// messaging.onBackgroundMessage(function (payload) {
+//   // console.log(defaultConfig);
+//   console.log("Received background message");
+//   console.log(payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image,
-  };
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: payload.notification.image,
+//   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
