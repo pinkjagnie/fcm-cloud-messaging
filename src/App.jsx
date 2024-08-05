48fd14a25919694808a00a10f3143d0a35f957d9
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-05 13:20:34 +0200

    Set up asking for user’s permission

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { getToken } from "firebase/messaging";
+import { messaging } from "./firebase/firebaseConfig";
+
+const { VITE_APP_VAPID_KEY } = import.meta.env;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  async function requestPermission() {
+    //requesting permission using Notification API
+    const permission = await Notification.requestPermission();
+
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey: VITE_APP_VAPID_KEY,
+      });
+
+      //consoling the token
+      console.log("Token generated : ", token);
+    } else if (permission === "denied") {
+      //notifications are blocked
+      alert("You denied for the notification");
+    }
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
 
   return (
     <>
@@ -29,7 +55,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
