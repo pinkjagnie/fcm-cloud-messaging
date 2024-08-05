6f89ae2681107a2a2515ca8e9197ba22b5f1f935
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-05 14:12:07 +0200

    Set up background notification listener

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+// Initialize the Firebase app in the service worker
+// "Default" Firebase configuration (prevents errors)
+const defaultConfig = {
+  apiKey: true,
+  projectId: true,
+  messagingSenderId: true,
+  appId: true,
+};
+
+firebase.initializeApp(defaultConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+//Listens for background notifications
+messaging.onBackgroundMessage((payload) => {
+  console.log("Received background message");
+  console.log(payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
