bbcf0758fdfc1b9310abcdfa170a79fa2b881cbe
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-06 17:01:48 +0200

    Fixed issues with functions

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const { onRequest } = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -23,6 +23,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.sendNotification = functions.https.onRequest(async (req, res) => {
+  // eslint-disable-next-line object-curly-spacing
   const { token, title, body } = req.body;
 
   const message = {
