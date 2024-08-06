d7e72ab5290f01f34c1f4f2855f7496f121f8929
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-06 16:04:52 +0200

    Configured Firebase functions

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -17,3 +17,26 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.sendNotification = functions.https.onRequest(async (req, res) => {
+  const { token, title, body } = req.body;
+
+  const message = {
+    notification: {
+      title,
+      body,
+    },
+    token,
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    res.status(200).send("Notification sent successfully: " + response);
+  } catch (error) {
+    res.status(500).send("Error sending notification: " + error);
+  }
+});
